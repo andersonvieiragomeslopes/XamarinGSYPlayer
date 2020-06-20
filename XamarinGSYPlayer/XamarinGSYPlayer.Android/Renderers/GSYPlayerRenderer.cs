746fa384481113ff0a2544a3325727172a438f41
anderson vieira gomes <andersonvieira123456@gmail.com>
2020-06-20 06:11:30 -0300

    finish part one

--- XamarinGSYPlayer/XamarinGSYPlayer.Android/Renderers/GSYPlayerRenderer.cs
+++ XamarinGSYPlayer/XamarinGSYPlayer.Android/Renderers/GSYPlayerRenderer.cs
@@ -10,19 +10,24 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Com.Shuyu.Gsyvideoplayer.Utils;
+using Com.Shuyu.Gsyvideoplayer.Video;
+using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using XamarinGSYPlayer.Controls;
-using ARelativeLayout = Android.Widget.RelativeLayout;
+using XamarinGSYPlayer.Droid.Renderers;
+[assembly: ExportRenderer(typeof(GSYPlayerControlView), typeof(GSYPlayerRenderer))]
 
 namespace XamarinGSYPlayer.Droid.Renderers
 {
     [Preserve(AllMembers = true)]
-
-    public class GSYPlayerRenderer : Xamarin.Forms.Platform.Android.AppCompat.ViewRenderer<GSYPlayerControlView, ARelativeLayout>
+    public class GSYPlayerRenderer : Xamarin.Forms.Platform.Android.AppCompat.ViewRenderer<GSYPlayerControlView, StandardGSYVideoPlayer>
     {
         Context context;
-        ARelativeLayout frameLayout;
         ViewGroup mainpage;
+        Com.Shuyu.Gsyvideoplayer.Utils.OrientationUtils orientationUtils;
+        StandardGSYVideoPlayer player;
+        ImageView imageView;
         public GSYPlayerRenderer(Context context) : base(context)
         {
             this.context = context;
@@ -35,30 +40,67 @@ namespace XamarinGSYPlayer.Droid.Renderers
             {
                 if (Control == null)
                 {
+                    player = new StandardGSYVideoPlayer(context);
                     InitializePlayer(e.NewElement.Souce);
 
-                    frameLayout = new ARelativeLayout(context);
-                    ARelativeLayout.LayoutParams layoutParams =
-                            new ARelativeLayout.LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent);
-                    layoutParams.AddRule(LayoutRules.CenterInParent);
+                    SetNativeControl(player);
+                    player.BackButton.Click += (sender, e) =>
+                    {
 
+                        try
+                        {
+                            player.SetUp("", true, "");
+                            player.OnBackFullscreen();
+
+
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                    };
+                    player.FullscreenButton.Click += (sender, e) =>
+                    {
+
+                        try
+                        {
+                            player.StartWindowFullscreen(context, true, true);
+
+
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                    };
+                    mainpage = ((ViewGroup)player.Parent);
 
                 }
+
             }
         }
+
         protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             base.OnElementPropertyChanged(sender, e);
         }
         protected override void Dispose(bool disposing)
         {
-            //mPlayerView?.Dispose();
-           // mPlayerView = null;
+            player?.Dispose();
+            player = null;
             base.Dispose(disposing);
 
         }
         private void InitializePlayer(string Source)
         {
+            imageView = new ImageView(context);
+
+            imageView.SetScaleType(ImageView.ScaleType.CenterCrop);
+            imageView.SetImageURI(Android.Net.Uri.Parse("https://hexoblogstorage.blob.core.windows.net/blogres/images/xamarin-custom-navbar-icon-text/navbardiff.png"));
+            player.ThumbImageView = imageView;
+            player.VerticalScrollBarEnabled = false;
+            player.SetUp(Source, true, "Video");
+            player.StartPlayLogic();
 
         }
 
