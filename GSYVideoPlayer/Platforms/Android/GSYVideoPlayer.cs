818bdca19681d45fe9920cd32f1541c1e1ff7c21
anderson vieira gomes <andersonvieira123456@gmail.com>
2020-06-21 08:12:41 -0300

    ok

--- GSYVideoPlayer/Platforms/Android/GSYVideoPlayer.cs
+++ GSYVideoPlayer/Platforms/Android/GSYVideoPlayer.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Plugin.GSYVideoPlayer.Platforms.Android;
+using Plugin.GSYVideoPlayer.Shared;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
+
 [assembly: ExportRenderer(typeof(SharedGSYVideoPlayer), typeof(GSYVideoPlayer))]
 
 namespace Plugin.GSYVideoPlayer.Platforms.Android
