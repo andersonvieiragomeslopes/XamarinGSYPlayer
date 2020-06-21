d020d2fbdb027ab942f6644e5ebd04b982a23378
anderson vieira gomes <andersonvieira123456@gmail.com>
2020-06-21 07:58:06 -0300

    Init Plugin

---
+++ GSYVideoPlayer/Platforms/Android/GSYVideoPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+[assembly: ExportRenderer(typeof(SharedGSYVideoPlayer), typeof(GSYVideoPlayer))]
+
+namespace Plugin.GSYVideoPlayer.Platforms.Android
+{
+    public class GSYVideoPlayer
+    {
+    }
+}
